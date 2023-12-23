c4a36b45bc20e9b10f99bb9e61edd40426ea3bc5
Samba@WS <santos.diogo.21@outlook.com>
2023-12-23 21:23:52 +0000

    Codigo para criacao do pacote cliente

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,6 +1,16 @@
 package Client;
 
+import Protocol.Exec.Request;
+
 public class Client 
 {
-    
+    /**
+     * Produces a job to send to the Server
+     * @param job
+     */
+    Request sendExec (byte[] job)
+    {
+        Request r= new Request(job);
+        return r;
+    }
 }

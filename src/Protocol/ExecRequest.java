e10be5c52796b678949797ff78cd0f1ef2d1d738
Samba@WS <santos.diogo.21@outlook.com>
2023-12-23 16:40:31 +0000

    comeco

--- src/Protocol/ExecRequest.java
+++ src/Protocol/ExecRequest.java
@@ -2,6 +2,13 @@ package Protocol;
 
 public class ExecRequest extends Protocol
 {
+    public byte[] arg;                  // Arguments of the exec
+    public int nJob;                    // Number of the job
+    public static int inc= 0;           // Incrementing the number of the job
 
-    
+    public ExecRequest (byte[] arg)
+    {
+        super (Type.EXEC_RQ);
+        this.nJob= inc++;
+    }
 }

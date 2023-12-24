a970ff2547d6d8211f83b8a9689dbec70598b018
Luís Ribeiro <a100608@alunos.uminho.pt>
2023-12-24 18:16:11 +0000

    Parte do handler de receber e tratar pedidos feita

--- src/Server/Handler.java
+++ src/Server/Handler.java
@@ -1,8 +1,12 @@
 package Server;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
 import java.net.Socket;
 import java.util.concurrent.BlockingQueue;
 import sd23.*;
+import Protocol.Protocol;
 import Protocol.Exec.Request;
 import Protocol.Status.StatusREP;
 import Protocol.Status.StatusREQ;
@@ -12,7 +16,8 @@ public class Handler implements Runnable
 
     private MemoryManager memoryManager;
     private BlockingQueue<Task> taskQueue;
-    private Socket clientSocket; 
+    private Socket clientSocket;
+    private DataInputStream in;
     
     
     public Handler (MemoryManager memoryManager, BlockingQueue<Task> taskQueue, Socket clientSocket)
@@ -24,13 +29,14 @@ public class Handler implements Runnable
 
 
     // Método para tratar requisições de status. Retorna uma resposta com o estado atual do serviço.
-    public StatusREP handleStatusRequest(StatusREQ request) {
+    // Não aceita nada por parametro pois o StatusREQ (para já pelo menos) não tem qualquer conteúdo
+    private StatusREP handleStatusRequest() {
         long availableMemory = memoryManager.getAvailableMemory();
         int pendingTasks = taskQueue.size();
         return new StatusREP(availableMemory, pendingTasks);
     }
 
-    public void handleExec (Request packet)
+    private void handleExec (Request packet)
     {
         try 
         {
@@ -44,8 +50,36 @@ public class Handler implements Runnable
         }
     }
 
+
+    private void handle (Protocol packet)
+    {
+        try {
+            switch (packet.type) {
+                case EXEC_RQ:
+                    handleExec(Request.deserialize(this.in));
+                    break;
+                case STATUS_RQ:
+                    handleStatusRequest();
+                    break;
+                default:
+                    break;
+            }
+        } catch (IOException e) {
+            System.err.println("Error deserializing packet");
+        }
+    }
+
+
     public void run()
     {
-        
+        try {
+            in = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()));
+            while(true)
+            {
+                handle(Protocol.deserialize(in));
+            }
+        } catch (IOException e) {
+            System.err.println("fds");
+        }
     }
 }

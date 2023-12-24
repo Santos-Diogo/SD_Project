d1b5fb19ca4a54967d7823e4455993bdb46f49af
Luís Ribeiro <a100608@alunos.uminho.pt>
2023-12-24 17:05:43 +0000

    Handler e adaptação do trabalho do Martim

--- src/Server/Handler.java
+++ src/Server/Handler.java
@@ -1,15 +1,38 @@
 package Server;
 
+import java.util.concurrent.BlockingQueue;
+import sd23.*;
 import Protocol.Exec.Request;
+import Protocol.Status.StatusREP;
+import Protocol.Status.StatusREQ;
 
 public class Handler 
 {
+
+    private MemoryManager memoryManager;
+    private BlockingQueue<Task> taskQueue;
+    
+    
+    public Handler (MemoryManager memoryManager, BlockingQueue<Task> taskQueue)
+    {
+        this.memoryManager = memoryManager;
+        this.taskQueue = taskQueue;
+    }
+
+
+    // Método para tratar requisições de status. Retorna uma resposta com o estado atual do serviço.
+    public StatusREP handleStatusRequest(StatusREQ request) {
+        long availableMemory = memoryManager.getAvailableMemory();
+        int pendingTasks = taskQueue.size();
+        return new StatusREP(availableMemory, pendingTasks);
+    }
+
     public void handleExec (Request packet)
     {
         try 
         {
             // execute task and get output
-            byte[] output = JobFunction.execute(packet.job);
+            byte[] output = JobFunction.execute(packet.arg);
         }
         catch (JobFunctionException e)
         {
--- src/Server/Main.java
+++ src/Server/Main.java
@@ -1,8 +1,14 @@
 package Server;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 import Protocol.Exec.Request;
 
 public class Main 
 {
-    
+
+    private MemoryManager memoryManager;  // Instância de MemoryManager para gerir a memória.
+    private BlockingQueue<Task> taskQueue = new LinkedBlockingQueue<>();
+
 }

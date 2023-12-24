e8263348493bc18b090e2ec5d8515a94461a15b2
Samba@WS <santos.diogo.21@outlook.com>
2023-12-24 16:16:21 +0000

    fix construtor

--- src/Protocol/Exec/Request.java
+++ src/Protocol/Exec/Request.java
@@ -7,15 +7,18 @@ import Protocol.Protocol;
 public class Request extends Protocol
 {
     public byte[] arg;                                      // Arguments of the exec
+    public int mem;                                         // Memory needed
     public int n_job;                                       // Number of the job
     private static int inc= 0;                              // Incrementing the number of the job
     private static ReentrantLock rl= new ReentrantLock();   // Used to ensure exclusion in the constructor
 
-    public Request (byte[] arg)
+    public Request (byte[] arg, int mem)
     {
         super (Type.EXEC_RQ);
         try
         {
+            this.arg= arg;
+            this.mem= mem;
             rl.lock();
             this.n_job= inc++;
         }

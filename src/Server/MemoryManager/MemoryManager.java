e473b34b163be37ff2850c9714747c2d176cfaa6
Samba@WS <santos.diogo.21@outlook.com>
2023-12-26 18:16:29 +0000

    mm

--- src/Server/MemoryManager/MemoryManager.java
+++ src/Server/MemoryManager/MemoryManager.java
@@ -1,6 +1,6 @@
 // Classe qie trata da ocupação de serviço
 
-package Server;
+package Server.MemoryManager;
 import java.lang.management.ManagementFactory;  // Interface para a gestão de memória em JAVA 
 import java.lang.management.MemoryMXBean;
 

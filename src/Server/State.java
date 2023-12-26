06279bf3cd8709acb2c01357505def47c9757c77
Samba@WS <santos.diogo.21@outlook.com>
2023-12-26 15:51:48 +0000

    Nocao de estado do servidor

--- src/Server/State.java
+++ src/Server/State.java
@@ -1,6 +1,16 @@
 package Server;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class State 
 {
-        
+    public MemoryManager memoryManager;
+    public BlockingQueue<Task> taskQueue;
+
+    public State ()
+    {
+        this.memoryManager= new MemoryManager();
+        this.taskQueue= new LinkedBlockingQueue<>();
+    }
 }

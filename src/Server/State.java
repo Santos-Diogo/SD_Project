d436aea7db70aaecf132f2500e26dd700c195055
Samba@WS <santos.diogo.21@outlook.com>
2023-12-26 16:09:33 +0000

    Definicao de alguns metodos do state

--- src/Server/State.java
+++ src/Server/State.java
@@ -2,15 +2,28 @@ package Server;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class State 
 {
-    public MemoryManager memoryManager;
-    public BlockingQueue<Task> taskQueue;
+    private ReentrantReadWriteLock mem_lock;                     // memory lock
+    public BlockingQueue<Task> taskQueue;                        // task queue
 
     public State ()
     {
-        this.memoryManager= new MemoryManager();
         this.taskQueue= new LinkedBlockingQueue<>();
+    }
+
+    public long getAvailableMemory ()
+    {
+        try
+        {
+            this.mem_lock.readLock().lock();
+            return MemoryManager.getAvailableMemory();
+        }
+        finally
+        {
+            this.mem_lock.readLock().unlock();
+        }
     }
 }

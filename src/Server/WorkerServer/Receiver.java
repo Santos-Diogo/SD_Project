69aebc2c057ddcba94cbe2245b6f10c4a5835180
Samba@WS <santos.diogo.21@outlook.com>
2023-12-28 22:30:23 +0000

    Implementacao basica do Receiver

---
+++ src/Server/WorkerServer/Receiver.java
@@ -0,0 +1,36 @@
+package Server.WorkerServer;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+import ThreadTools.ThreadControl;
+import Server.Shared.Packet.Packet;
+
+public class Receiver implements Runnable
+{
+    private ThreadControl tc;
+    private State state;
+    private DataInputStream input;
+
+    public Receiver (ThreadControl tc, State state, DataInputStream input)
+    {
+        this.state= state;
+        this.input= input;
+    }
+
+    public void run ()
+    {
+        while (this.tc.getRunning())
+        {
+            try
+            {
+                Packet p= Packet.deserialize(this.input);
+                this.state.inputQueue.add(p);
+            }
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+}
